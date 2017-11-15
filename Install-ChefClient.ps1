06882256e0a09faf9182878f26044ebafc714986
Trevor Hess <trevor.ghess@gmail.com>
2017-11-15 03:36:19 +0000

    rearrange tasks

--- Install-ChefClient.ps1
+++ Install-ChefClient.ps1
@@ -1,14 +1,15 @@
 Param(
     # Parameter help description
-    [Parameter(Mandatory=$true,Position=2)]
+    [Parameter(Mandatory=$true,Position=0)]
     [String]
     $ChefClientDownloadUrl
 )
 Write-Host "Downloading Chef Client from $ChefClientDownloadUrl" 
-Invoke-WebRequest -Uri $ChefClientDownloadUrl -OutFile "chef-client.msi" 
+$clientmsi = "chef-client.msi" 
+if(!Test-Path $clientmsi){
+    Invoke-WebRequest -Uri $ChefClientDownloadUrl -OutFile $clientmsi 
+}
 Write-Host "Installing Chef Client"
 Start-Process msiexec.exe -Wait -ArgumentList '/I chef-client.msi /qn ADDLOCAL="ChefClientFeature" ' -Verbose -NoNewWindow
-$splay = Get-Random -Minimum 30 -Maximum 900
-$T = New-ScheduledTaskTrigger -Once -At (Get-Date).AddSeconds($splay)
-Set-ScheduledTask Register-ChefNode -Trigger $T
+Start-ScheduledTask Register-ChefNode
 Unregister-ScheduledTask Install-ChefClient -Confirm:$false
--- Register-ChefNode.ps1
+++ Register-ChefNode.ps1
@@ -1,3 +1,4 @@
+Start-Sleep -Seconds $(Get-Random -Minimum 30 -Maximum 300)
 Write-Host "Running Chef Client"
 Start-Process cmd.exe -Wait '/c "c:\opscode\chef\bin\chef-client.bat -j c:\chef\first-boot.json"'
 Unregister-ScheduledTask Register-ChefNode -Confirm:$false
